a1b82ada9b83b69a9e70d3360ba2be48d60e31ff
Dhakshnan <150678283+Dhakshnan@users.noreply.github.com>
2025-05-23 06:17:23 -0700

    Update SkillsSection.jsx

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 
+
 const skills = [
   // Frontend
-  { name: "HTML5", category: "frontend" },
-  { name: "CSS3", category: "frontend" },
-  { name: "JavaScript", category: "frontend" },
-  { name: "React Js", category: "frontend" },
-  { name: "Tailwind CSS", category: "frontend" },
-  { name: "Json", category: "frontend" },
+  { name: "HTML5", category: "frontend", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},
+  { name: "CSS3", category: "frontend", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+  { name: "JavaScript", category: "frontend", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+  { name: "React Js", category: "frontend",  icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+  { name: "Tailwind CSS", category: "frontend", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg" },
+  { name: "Json", category: "frontend", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/json/json-original.svg" },
   
   
   // CMS Tools
-  { name: "AEM Content Author", category: "CMS Tools" },
-  { name: "ICMS", category: "CMS Tools" },
-  { name: "Citi Drupal", category: "CMS Tools" },
+  { name: "AEM Content Author", category: "CMS Tools", "icon": "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/adobe-experience-manager-aem-icon.svg" },
+  { name: "ICMS", category: "CMS Tools", icon: "https://cdn-icons-png.flaticon.com/512/4213/4213179.png" },
+  { name: "Citi Drupal", category: "CMS Tools", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/drupal/drupal-original.svg" },
 
   // Tools
-  { name: "GitHub", category: "tools" },
-  { name: "VS Code", category: "tools" },
+  { name: "GitHub", category: "tools", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" },
+  { name: "VS Code", category: "tools", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
 ];
 
 const categories = ["all", "frontend", "CMS Tools", "tools"];
 
 export const SkillsSection = () => {
-  const [activeCategory, setActiveCategory] = useState("all");
+  const [activeCategory, setActiveCategory] = useState("all")
 
   const filteredSkills = skills.filter(
     (skill) => activeCategory === "all" || skill.category === activeCategory
@@ -59,6 +60,13 @@ export const SkillsSection = () => {
               key={key}
               className="bg-card p-6 rounded-lg shadow-xs card-hover"
             >
+              {skill.icon && (
+    <img
+      src={skill.icon}
+      alt={skill.name}
+      className="w-10 h-10 object-contain mb-4"
+    />
+  )}
               <div className="text-left mb-4">
                 <h3 className="font-semibold text-lg"> {skill.name}</h3>
               </div>
